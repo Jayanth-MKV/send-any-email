b606fb116742f4e0f3611684bb06da263ba225da
Jayanth MKV <mkvjayanth1@gmail.com>
2024-03-30 16:11:03 +0530

    package

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import "./app.service";
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('send-any-mail')
-    .setDescription('The email sender')
-    .setVersion('0.1')
-    .build();
+  // const config = new DocumentBuilder()
+  //   .setTitle('send-any-mail')
+  //   .setDescription('The email sender')
+  //   .setVersion('0.1')
+  //   .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
 
 
-  await app.listen(3001);
+  // await app.listen(3001);
 }
 bootstrap();
